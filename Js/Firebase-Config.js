b6be065e356d73fd209e13e08252debdc480b1ee
yusufabduvaliev09 <yusufabduvaliev09@gmail.com>
2025-10-16 20:41:04 +0600

    Add Firebase configuration and initialization

---
+++ Js/Firebase-Config.js
@@ -0,0 +1,14 @@
+// ВСТАВЬ СВОЙ КОД FIREBASE СЮДА
+const firebaseConfig = {
+apiKey: «AIzaSyC8MMpKlitJtVBpVXn3PxD2yLzf19gRP9c»,
+authDomain: «abucargo-c7638.firebaseapp.com»,
+databaseURL: «http://abucargo-c7638.firebasestorage.app&#187;,
+projectId: «abucargo-c7638»,
+storageBucket: «abucargo-c7638.firebasestorage.app»,
+messagingSenderId: «975467994957»,
+appId: «1:975467994957:web:59782c2387dca45ce8a104»
+};
+
+// инициализация
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
