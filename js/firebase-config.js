f407c1df956921d6ae1f83d1575a7c45e3a078db
yusufabduvaliev09 <yusufabduvaliev09@gmail.com>
2025-10-16 22:48:09 +0600

    Fix databaseURL in firebase config
    Updated the databaseURL to point to the correct Firebase Realtime Database.

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -2,7 +2,7 @@
 const firebaseConfig = {
 apiKey: «AIzaSyC8MMpKlitJtVBpVXn3PxD2yLzf19gRP9c»,
 authDomain: «abucargo-c7638.firebaseapp.com»,
-databaseURL: «http://abucargo-c7638.firebasestorage.app&#187;,
+databaseURL: «http://abucargo-c7638-default-rtdb.firebasestorage.app&#187;,
 projectId: «abucargo-c7638»,
 storageBucket: «abucargo-c7638.firebasestorage.app»,
 messagingSenderId: «975467994957»,
